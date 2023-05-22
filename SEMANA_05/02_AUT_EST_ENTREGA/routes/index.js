63665a03ec362a994d860e4b8a1284fa62aa0adc
Felipe Liberman Fuchs <felipe@almosthackers.io>
2023-05-22 09:31:55 -0300

    create basic curriculum layout

--- SEMANA_05/02_AUT_EST_ENTREGA/routes/index.js
+++ SEMANA_05/02_AUT_EST_ENTREGA/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Project' });
+  res.render('index', { title: 'Felipe L. Fuchs' });
 });
 
 module.exports = router;
