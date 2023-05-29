196acd2922415626be45ec452d1239a8fb15ce02
Felipe Liberman Fuchs <felipe@almosthackers.io>
2023-05-29 10:02:06 -0300

    use JQuery to create a Show More button

---
+++ SEMANA_06/02_AUT_EST_ENTREGA/public/javascripts/application.js
@@ -0,0 +1,11 @@
+if ($('.collapsible-list').length > 3) {
+    $('.collapsible-list:gt(2)').hide();
+    $('.btn-show-more').show();
+}
+
+$('.btn-show-more').on('click', function () {
+    //toggle elements with class .collapsible-list that their index is bigger than 2
+    $('.collapsible-list:gt(2)').toggle();
+    //change text of show more element just for demonstration purposes to this demo
+    $(this).text() === 'Ver Mais' ? $(this).text('Ver Menos') : $(this).text('Ver Mais');
+});
